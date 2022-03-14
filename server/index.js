6420e5ca76e0d8820ec4aa4d145eaafcbd4fb9c6
Julian Boetto <julib_8724@hotmail.com>
2022-03-14 12:01:51 -0300

    Localhost retorna o html sem arquivos estáticos

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+import config from './config.js'
 import server from "./server.js";
+import { logger } from './util.js'
 
 server.listen(3000)
-.on('listening', () => console.log('Server funcionando!'))
+.on('listening', () => logger.info(`Server funcionando no ${config.port}!!`))
