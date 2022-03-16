32ac26970678c8c08fa8f11589abc7a8567a3bbb
Julian Boetto <julib_8724@hotmail.com>
2022-03-16 10:30:29 -0300

    Controller 100% testes prontos

---
+++ tests/unit/server/controller.test.js
@@ -0,0 +1,42 @@
+import { 
+    jest,
+    expect,
+    describe,
+    test,
+    beforeEach
+} from '@jest/globals'
+import config from '../../../server/config.js'
+import { Controller } from '../../../server/controller.js'
+import { Service } from '../../../server/service.js'
+import TestUtil from '../_util/testUtil.js'
+const { pages } = config
+
+describe('#controller - test site for controller return', () => {
+    beforeEach(() => {
+        jest.restoreAllMocks()
+        jest.clearAllMocks
+    })
+
+    test('getFileStream - should return a file stream', async () => {
+        const mockFileStream = TestUtil.generateReadableStream(['data'])
+        const expectedType = '.html'
+
+        const mockResolved = jest.spyOn(
+            Service.prototype,
+            Service.prototype.getFileStream.name
+        ).mockResolvedValue({
+            stream: mockFileStream,
+            type: expectedType
+        })
+
+        const controller = new Controller()
+        const controllerReturn = await controller.getFileStream('./index.html')
+
+        expect(mockResolved).toBeCalledWith('./index.html')
+        expect(controllerReturn).toStrictEqual({
+            stream: mockFileStream,
+            type: expectedType
+        })
+    })
+
+})
